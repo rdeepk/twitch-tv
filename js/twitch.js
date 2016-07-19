6a315cb90c8bf79ac756815530eeb7d65dc5a4aa
Ramandeep Kaur <rdeepk@gmail.com>
2016-07-18 20:11:52 -0700

    Responsive layout implemented

--- js/twitch.js
+++ js/twitch.js
@@ -1,6 +1,7 @@
 var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","brunofin","comster404","test_channel"];
 var status, name, game, description, followers, viewers, views, created_at, url;
 var blank = "&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;";
+var defaultLogo = "http://www.windowscentral.com/sites/wpcentral.com/files/styles/thumbnail/public/topic_images/2016/twitch-logo-topic.png";
 
 function getRandomChannels() {
 	url = "https://api.twitch.tv/kraken/streams?limit=10&offset=%RAND";
@@ -17,10 +18,10 @@ function getChannelData() {
 	channels.forEach( function( item ) {
 	url = "https://api.twitch.tv/kraken/streams/" + item;
 	$.getJSON( url, function (response) {
-		setDefaults(item);
 		 if ( response.stream === null ) {
 			status = "Offline";
 		}
+		setDefaults(item);
 		setData(response.stream);
 		displayData();
 		}).fail(function(jqXHR, textStatus, errorThrown) {
@@ -35,10 +36,10 @@ function getChannelData() {
 
 function setData(response) {
 	if(response) {
-		logo = response.channel.logo;
+		logo = response.channel.logo ? response.channel.logo : defaultLogo;
 		name = response.channel.display_name;
 		game = response.channel.game;
-		status = response.channel.status;
+		status = response.channel.status.trunc(50);
 		description = game + ": " + status;
 		followers = response.channel.followers;
 		viewers = response.viewers;
@@ -48,31 +49,43 @@ function setData(response) {
 	}
 }
 
+String.prototype.trunc = String.prototype.trunc ||
+function(n){
+	return (this.length > n) ? this.substr(0,n-1)+'&hellip;' : this;
+};
+
 function displayData() {
-	var headerClass;
-	if( status === "Offline" ) {
-		headerClass = "offline";
-	} else if ( status === "Account Closed" ) {
-		headerClass = "account-closed";
-	} else {
-		headerClass = "";
-	}
-	html = '<div class="channel"><div class="row"><div class="col-sm-12"><div class="logo"><img src="' +
-	logo + '"></div><div class="header-text ' + headerClass + '"><h2>' +
+	var headerClass = getHeaderClass();	
+	html = '<div class="channel ' + headerClass + '"><div class="row"><div class="col-sm-12"><div class="logo"><img src="' +
+	logo + '"></div><div class="header-text"><h2>' +
 	name + '</h2><p class="description">' +
 	description + '</p><a href="' + url + '" class="url">' +
-	url + '</a></div></div></div><div class="row"><div class="col-sm-8"><dl><dt>Game</dt><dd>: ' +
-	game + '</dd><dt>Status</dt><dd>: ' +
-	status + '</dd><dt>Viewers</dt><dd>: ' +
-	viewers + '</dd><dt>Streaming Since</dt><dd>: ' +
-	created_at + '</dd><dt>Channel Views</dt><dd>: ' +
-	views + '</dd></dl></div><div class="col-sm-4"><div class="followers"><p class="value">' +
+	url + '</a></div></div></div><div class="row"><div class="col-sm-8"><dl><dt>Game</dt><dd><span class="seperator">: </span>' +
+	game + '</dd><dt>Status</dt><dd><span class="seperator">: </span>' +
+	status + '</dd><dt>Viewers</dt><dd><span class="seperator">: </span>' +
+	viewers + '</dd><dt>Streaming Since</dt><dd><span class="seperator">: </span>' +
+	created_at + '</dd><dt>Channel Views</dt><dd><span class="seperator">: </span>' +
+	views + '</dd></dl><div class="clearfix"></div></div><div class="col-sm-4"><div class="followers"><p class="value">' +
 	followers + '</p><p class="title">Followers</p></div></div></div>';
-	$( ".content" ).append( html );
+	if( headerClass == "" ) {
+		$( ".content" ).prepend( html );
+	} else {
+		$( ".content" ).append( html );
+	}
+}
+
+function getHeaderClass() {
+	if( status === "Offline" ) {
+		return "offline";
+	} else if ( status === "Account Closed" ) {
+		return "account-closed";
+	} else {
+		return "";
+	}
 }
 
 function setDefaults(item) {
-	logo = "http://www.windowscentral.com/sites/wpcentral.com/files/styles/thumbnail/public/topic_images/2016/twitch-logo-topic.png";
+	logo = defaultLogo;
 	name = item;
 	url = "https://www.twitch.tv/" + name;
 	game = blank;
