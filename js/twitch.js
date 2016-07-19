35d471ec80d0f2edcb062f1b1fc45aa0031d2387
Ramandeep Kaur <rdeepk@gmail.com>
2016-07-19 14:15:12 -0700

    Documentation Added

--- js/twitch.js
+++ js/twitch.js
@@ -3,39 +3,50 @@ var status, name, game, description, followers, viewers, views, created_at, url;
 var blank = "&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;";
 var defaultLogo = "http://www.windowscentral.com/sites/wpcentral.com/files/styles/thumbnail/public/topic_images/2016/twitch-logo-topic.png";
 
-function getRandomChannels() {
+/**
+ * @summary Fetch the random channels from Twitch API and initiates the display.
+ */
+function displayRandomChannels() {
 	url = "https://api.twitch.tv/kraken/streams?limit=10&offset=%RAND";
-	$.getJSON( url, function (response) {
+	$.getJSON( url, function( response ) {
 		response.streams.forEach( function( item ) {
-			setData(item);
+			setData( item );
 			displayData();
 		});		
 	});
-	getChannelData();
 }
 
-function getChannelData() {
+/**
+ * @summary Wrapper function to start the process of getting channels data and display them.
+ */
+function displayChannelData() {
+	displayRandomChannels();
 	channels.forEach( function( item ) {
 	url = "https://api.twitch.tv/kraken/streams/" + item;
-	$.getJSON( url, function (response) {
-		 if ( response.stream === null ) {
+	$.getJSON( url, function( response ) {
+		if ( response.stream === null ) {
 			status = "Offline";
 		}
-		setDefaults(item);
-		setData(response.stream);
+		setDefaults( item );
+		setData( response.stream );
 		displayData();
-		}).fail(function(jqXHR, textStatus, errorThrown) {
-    if(errorThrown == "status code 422") {
-			status = "Account Closed";
-			setDefaults(item);
-			displayData();
-		}
-  });
+		}).fail( function( jqXHR, textStatus, errorThrown ) {
+			if( errorThrown == "status code 422" ) {
+				status = "Account Closed";
+				setDefaults( item );
+				displayData();
+			}
+  	});
 	});
 }
 
-function setData(response) {
-	if(response) {
+/**
+ * @summary Sets the global data with the values from response object from Twitch API.
+ *
+ * @param object $response Response object from Twitch API.
+ */
+function setData( response ) {
+	if( response ) {
 		logo = response.channel.logo ? response.channel.logo : defaultLogo;
 		name = response.channel.display_name;
 		game = response.channel.game;
@@ -49,11 +60,21 @@ function setData(response) {
 	}
 }
 
+/**
+ * @summary Prototype for truncating the string.
+ *
+ * @param number $n Character count for truncated string.
+ *
+ * @return string Truncated string.
+ */
 String.prototype.trunc = String.prototype.trunc ||
-function(n){
-	return (this.length > n) ? this.substr(0,n-1)+'&hellip;' : this;
+function( n ){
+	return ( this.length > n ) ? this.substr( 0, n-1 ) + '&hellip;' : this;
 };
 
+/**
+ * @summary Sets and display the HTML for each channel.
+ */
 function displayData() {
 	var headerClass = getHeaderClass();	
 	html = '<div class="channel ' + headerClass + '"><div class="row"><div class="col-sm-12"><div class="logo"><img src="' +
@@ -74,6 +95,11 @@ function displayData() {
 	}
 }
 
+/**
+ * @summary Decides the class to be applied on Header of the Channels.
+ *
+ * @return string Class name to be set to channel header.
+ */
 function getHeaderClass() {
 	if( status === "Offline" ) {
 		return "offline";
@@ -84,7 +110,10 @@ function getHeaderClass() {
 	}
 }
 
-function setDefaults(item) {
+/**
+ * @summary Sets the default values for global variables.
+ */
+function setDefaults( item ) {
 	logo = defaultLogo;
 	name = item;
 	url = "https://www.twitch.tv/" + name;
@@ -96,4 +125,4 @@ function setDefaults(item) {
 	created_at = blank;
 }
 
-getRandomChannels();
+displayChannelData();
